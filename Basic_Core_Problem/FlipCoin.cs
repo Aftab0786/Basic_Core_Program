1ba3b93f57c40f7998baba14356c9dd826383825
Aftab-1998 <alam41976@gmail.com>
2022-01-27 15:46:36 +0530

    [AFTAB] Add; Added the welcome

---
+++ Basic_Core_Problem/FlipCoin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core_Problem
+{
+    internal class FlipCoin
+    {
+    }
+}
